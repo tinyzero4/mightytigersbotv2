590ff1f6249b41b6917fe2a79d2affda6296c84d
dmitry <dzmitry.zenin@gmail.com>
2023-06-14 21:42:38 +0200

    minor

--- lib/app/service/useCases.ts
+++ lib/app/service/useCases.ts
@@ -1,14 +1,14 @@
 import { Telegraf } from 'telegraf';
 import { Team } from '../domain/team';
 import { DataRepository } from '@app/service/dataRepository';
+import { TEAM_DEFAULT_SCHEDULE } from '@app/environment/appConfig';
 
 export class UseCases {
 
     private readonly repository = new DataRepository();
 
     async initTeam(id: string, name: string): Promise<Team> {
-        const team = this.repository.initTeam({id, name, created: new Date()})
-        return team;
+        return this.repository.initTeam({id, name, created: new Date(), schedule: TEAM_DEFAULT_SCHEDULE});
     }
 
     async onMessage() {
